ec28c26db52e1d3e9ae101ac9c56ff75400f2eb1
rakib-hasan <mahmud1807077@stud.kuet.ac.bd>
2022-05-17 05:05:36 +0600

    feature2 commit

---
+++ app/src/main/java/com/example/calculator/feature2.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+public class feature2 {
+    int b;
+
+    public feature2(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
